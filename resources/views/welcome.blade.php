1bbcce0e5fef55502281e54dbf3730da9c3a59c4
vagrant <vagrant@homestead>
2020-01-27 16:22:14 +0000

    small changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,60 +13,30 @@
     <div class="container-fluid">
         <header>
             <nav class="navbar navbar-light bg-light">
-                <span class="navbar-brand mb-0 h1">WILLIAMSBURG
-                North 6th Street</span>
+                <span class="navbar-brand mb-0 h1">
+                WILLIAMSBURG
+                North 6th Street
+                </span>
             </nav>
         </header>
 
-
-
-        <div id="carousel" class="row carousel slide bg-primary" data-ride="carousel">
-
-            <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class=""></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1" class=""></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2" class=""></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="3" class=""></li>
-            </ol>
-
-            <div class="carousel-inner" >
-                <div class="carousel-item center-block active">
-                    <img class="d-block img-fluid" src="{{ asset('images/3.jpg') }}" alt="First slide">
-                </div>
-                <div class="carousel-item ">
-                    <img class="d-block img-fluid" src="{{ asset('images/1.jpg') }}" alt="Second slide">
-                </div>
-                <div class="carousel-item ">
-                    <img class="d-block img-fluid" src="{{ asset('images/2.jpg') }}">
-                </div>
-                <div class="carousel-item ">
-                    <img class="d-block img-fluid" src="{{ asset('images/4.jpg') }}">
-                </div>
-            </div>
-            <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon text-primary" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next text-primary" href="#carousel" role="button" data-slide="next">
-                <span class="carousel-control-next-icon text-primary" aria-hidden="true"></span>
-                <span class="sr-only text-primary">Next</span>
-            </a>
+        {{--main picture--}}
+        <div class="carousel-item ">
+            <img class="d-block img-fluid" src="{{ asset('images/4.jpg') }}">
         </div>
 
         <div class="row">
             <div class="jumbotron col-12 bg-info mb-0">
                 <div class="title m-b-md display-3 text-center text-light underlinecolor">
-                    RETAIL SPACE AVAILABLE 2019
+                    RETAIL SPACE AVAILABLE
+                    2020
                 </div>
 
                     <h1 class="text-center text-light">
-                        A Total Of 14,100 Sq. Ft. With 75 Foot Frontage
+                        61N 6th Ground Level 4,200 sq.ft with 21.5 ft ceiling height/basement level 3,300 sq ft with 10 ft ceiling height, $300 per sq. ft.
                     </h1>
                     <h1 class="text-center text-light">
-                        61 N. 6th - Ground Level 4,200 Sq. Ft. with 21.5 Ft. Ceiling Height / Basement Level 3,300 Sq. Ft. with 10 Ft. Ceiling Height / $250 per Sq. Ft.
-                    </h1>
-                    <h1 class="text-center text-light">
-                        63 N. 6th - Ground Level 3,300 Sq. Ft. with 20.5 Ft. Ceiling Height / Basement Level 3,300 Sq. Ft. with 10 Ft Ceiling Height / $250 per Sq. Ft.
+                        63N 6th Ground level 3,300 sq.ft. with 20 sq ft ceiling height/basement level 3,300 sq. ft. with 10 ft ceiling height, 300$ per sq. ft.
                     </h1>
             </div>
         </div>
