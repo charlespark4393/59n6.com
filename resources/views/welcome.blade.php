3ac9409192f418be35b7b6d33899e1bfd6f4dd2b
vagrant <vagrant@homestead>
2020-02-07 19:20:55 +0000

    lineheight

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,9 +30,9 @@
         <div class="row">
             <div class="jumbotron col-12 mb-0">
                 <div class="display-3 text-center text-dark">
-                    <h1><span style="font-size:2.0rem;letter-spacing:-2px;">RETAIL</span></h1>
-                    <h1><span style="font-size:2.0rem;letter-spacing:-2px;">FOR LEASE</span></h1>
-                    <h1><span style="font-size:2.0rem;letter-spacing:-2px;"><a href="tel:19147609600">914-760-9600</a></span></h1>
+                    <h1 class="mb-0"><span style="font-size:2.0rem;letter-spacing:-2px;line-height:.7;">RETAIL</span></h1>
+                    <h1 class="mb-0"><span style="font-size:2.0rem;letter-spacing:-2px;line-height:.7;">FOR LEASE</span></h1>
+                    <h1><span style="font-size:2.0rem;letter-spacing:-2px;line-height:.7;"><a href="tel:19147609600">914-760-9600</a></span></h1>
                 </div>
 
                     <h1 class="text-center text-dark">
