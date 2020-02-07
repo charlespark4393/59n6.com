33318af1ee3466484e2051b8a71bc87993a98c13
vagrant <vagrant@homestead>
2020-02-07 19:26:21 +0000

    lineheight

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,9 +30,9 @@
         <div class="row">
             <div class="jumbotron col-12 mb-0">
                 <div class="display-3 text-center text-dark" style="font-size:2.0rem;letter-spacing:-2px;line-height:.7 !important;">
-                    <h1 class="mb-0" style="line-height:.7;"><span style="font-size:2.0rem;letter-spacing:-2px;line-height:.7 !important;">RETAIL</span></h1>
-                    <h1 class="mb-0" style="line-height:.7;"><span style="font-size:2.0rem;letter-spacing:-2px;line-height:.7 !important; ">FOR LEASE</span></h1>
-                    <h1><span style="font-size:2.0rem;letter-spacing:-2px;line-height:.7 !important;"><a href="tel:19147609600">914-760-9600</a></span></h1>
+                    <h1 class="mb-0" style="line-height:.6;"><span style="font-size:2.0rem;letter-spacing:-2px;line-height:.7 !important;">RETAIL</span></h1>
+                    <h1 class="mb-0" style="line-height:.6;"><span style="font-size:2.0rem;letter-spacing:-2px;line-height:.7 !important; ">FOR LEASE</span></h1>
+                    <h1 style="line-height:.6;"><span style="font-size:2.0rem;letter-spacing:-2px;line-height:.7 !important;"><a href="tel:19147609600">914-760-9600</a></span></h1>
                 </div>
 
                     <h1 class="text-center text-dark">
